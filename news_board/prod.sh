d82e15fd0ab97e0783cc44c533e2c3a266e8ca1a
rostislav <rostmoguchiy@gmail.com>
2020-07-25 13:16:59 +0300

    fix: dockerfile

--- news_board/prod.sh
+++ news_board/prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #gunicorn -b 0.0.0.0:8000 news_board.wsgi
-uwsgi --http :$PORT --wsgi-file news_board/wsgi.py  # faster option than gunicorn
+uwsgi --http :8000 --wsgi-file news_board/wsgi.py  # faster option than gunicorn
